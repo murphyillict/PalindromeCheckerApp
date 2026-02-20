f269487ccbb60af3d4101a525d09302d217a8e7f
Prajan_v <pv8673@srmist.edu.in>
2026-02-20 09:30:05 +0530

    Add PalindromeCheckerApp source file

---
+++ Palindrome App/src/PalindromeCheckerApp.java
@@ -0,0 +1,35 @@
+/**
+ * MAIN CLASS - UseCase1PalindromeApp
+ *
+ * Use Case 1: Application Entry And Welcome Page
+ *
+ * Description:
+ * This Class represents the entry point of the
+ * Palindrome Checker Management System
+ *
+ * At this stage, the Application:
+ * - Starts execution from the main() method
+ * - Displays a welcome message
+ * - Shows application version
+ *
+ * No palindrome logic is implemented yet.
+ *
+ * The goal is to establish a clear startup flow.
+ *
+ * @author Developer
+ * @version 1.0
+ */
+
+public class PalindromeCheckerApp{
+    /**
+     * Application Entry Point.
+     *
+     * This is the first method executed by the JVM
+     * when the program starts.
+     *
+     * @param args Command line arguments
+     */
+    public static void main(String[] args){
+        System.out.println("Welcome to Palindrome Checker App Management System");
+    }
+}
