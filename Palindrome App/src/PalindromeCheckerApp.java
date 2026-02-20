5ac7e6e0445c5062e9dfd447830d4694c958de41
murphyillict <kspranjal31@gmail.com>
2026-02-20 10:30:01 +0530

    Add version and initialization message to main

--- Palindrome App/src/PalindromeCheckerApp.java
+++ Palindrome App/src/PalindromeCheckerApp.java
@@ -31,5 +31,7 @@ public class PalindromeCheckerApp{
      */
     public static void main(String[] args){
         System.out.println("Welcome to Palindrome Checker App Management System");
+                System.out.println("Version : 1.0");
+        System.out.println("System initialized successfully.");
     }
 }
